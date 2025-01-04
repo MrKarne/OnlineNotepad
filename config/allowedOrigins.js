4616b7c97c8282b374f9c9ad3c7a0a75ecd63adb
Maneeshwar <maneeshwarkarne@gmail.com>
2025-01-04 18:55:48 +0530

    Updated the allowed origins and node engine in package.json

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:8080',
-    'http://localhost'
+    'http://localhost',
+    'https://nodepad.glitch.me'
 ];
 
 module.exports = allowedOrigins
